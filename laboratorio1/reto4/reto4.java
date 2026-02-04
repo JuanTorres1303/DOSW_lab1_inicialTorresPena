f1d6475ec6506e546de904753ccd4f4be22e3eee
scalashildebrandop-dot <scalashildebrando.p@gmail.com>
2026-02-04 11:53:41 -0500

    reto 4 metodo crearhashmap con hashmap que ignora los duplicados

--- laboratorio1/reto4/reto4.java
+++ laboratorio1/reto4/reto4.java
@@ -1,1 +1,27 @@
- 
+import java.util.HashMap;
+import java.util.List;
+
+public class reto4 {
+    
+    static class Par {
+        String clave;
+        Integer valor;
+        
+        public Par(String clave, Integer valor) {
+            this.clave = clave;
+            this.valor = valor;
+        }
+    }
+    
+    public static HashMap<String, Integer> crearHashMap(List<Par> listaPares) {
+        HashMap<String, Integer> mapa = new HashMap<>();
+        
+        for (Par par : listaPares) {
+            if (!mapa.containsKey(par.clave)) {
+                mapa.put(par.clave, par.valor);
+            }
+        }
+        
+        return mapa;
+    }
+}
